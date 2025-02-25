7ef5932acce36f951feb727acf229748bfb056a0
Lutfi762 <148749041+Lutfi762@users.noreply.github.com>
2025-02-25 20:56:51 +0700

    Pesan commit yang jelas

--- app/Http/Controllers/Apps/PermissionController.php
+++ app/Http/Controllers/Apps/PermissionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Apps;
 
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Spatie\Permission\Models\Permission;
---
+++ app/Models/Permission.php
@@ -0,0 +1,8 @@
+namespace App\Models;
+
+use Spatie\Permission\Models\Permission as SpatiePermission;
+
+class Permission extends SpatiePermission
+{
+    
+}
